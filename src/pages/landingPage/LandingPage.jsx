0f41896f449ad28d35b2fb7acc779ffee700cc1c
Xusniddin <narkulof@gmail.com>
2022-10-16 14:55:57 +0500

    choose categories section completed with responsive

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Accessories from "./accessories/Accessories";
 import Advantages from "./advantages/Advantages";
 import BestSeller from "./bestSeller/BestSeller";
+import Categories from "./categories/Categories";
 import FeaturedProducts from "./featuredProducts/FeaturedProducts";
 import Hero from "./header/Hero";
 import NewArrivals from "./newArrivals/NewArrivals";
@@ -15,6 +16,7 @@ const LandingPage = () => {
          <BestSeller />
          <NewArrivals />
          <Advantages />
+         <Categories />
       </>
    );
 };
---
+++ src/pages/landingPage/categories/Categories.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./categories.scss";
+
+import ellips1 from "./img/Ellipse32.png";
+import ellips2 from "./img/Ellipse33.png";
+import ellips3 from "./img/Ellipse34.png";
+import sneaker from "./img/sneaker.png";
+import football from "./img/football1.png";
+import volleyball from "./img/volley1.png";
+
+const prods = [
+   {
+      category: "Sneakers",
+      items: 120,
+      ellips: ellips1,
+      img: sneaker,
+   },
+   {
+      category: "Football",
+      items: 80,
+      ellips: ellips2,
+      img: football,
+   },
+   {
+      category: "Volleyball",
+      items: 160,
+      ellips: ellips3,
+      img: volleyball,
+   },
+   // {
+   //    category: "Sneakers",
+   //    items: 120,
+   //    ellips: ellips1,
+   //    img: sneaker,
+   // },
+   // {
+   //    category: "Football",
+   //    items: 80,
+   //    ellips: ellips2,
+   //    img: football,
+   // },
+   // {
+   //    category: "Volleyball",
+   //    items: 160,
+   //    ellips: ellips3,
+   //    img: volleyball,
+   // },
+];
+
+const Categories = () => {
+   return (
+      <section className="categories">
+         <div className="categories__head">
+            <h2>Choose categories</h2>
+
+            <p>
+               For explosive events (sprints up to 400 metres, long jump, triple
+               jump) the reduction in atmospheric pressure means there is less
+               resistance from the atmosphere.
+            </p>
+         </div>
+
+         <div className="categories__body">
+            {prods.map(({ category, items, ellips, img }, i) => {
+               return (
+                  <div className={`categories__body--card ${category}`} key={i}>
+                     <h3>{category} Collection</h3>
+
+                     <p>{items} products</p>
+
+                     <b>See collection</b>
+
+                     <img
+                        className="categories__body--card--img"
+                        src={img}
+                        alt={category}
+                     />
+
+                     <img
+                        className="categories__body--card--decoration"
+                        src={ellips}
+                        alt="decoration"
+                     />
+                  </div>
+               );
+            })}
+         </div>
+      </section>
+   );
+};
+
+export default Categories;
