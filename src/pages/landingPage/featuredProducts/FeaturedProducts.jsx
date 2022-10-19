821a9b05a629a469138e23d566e385105a639a77
Xusniddin <narkulof@gmail.com>
2022-10-19 16:55:24 +0500

    featured products style fixed

--- src/pages/landingPage/featuredProducts/FeaturedProducts.jsx
+++ src/pages/landingPage/featuredProducts/FeaturedProducts.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDraggable } from "react-use-draggable-scroll";
 
 import "./featuredProducts.scss";
 
@@ -9,7 +10,34 @@ import img4 from "./img/image44.png";
 import { RightArrow, StarFilled } from "../../../components/icons/Icons";
 
 const FeaturedProducts = () => {
+   const ref = useRef();
+   const { events } = useDraggable(ref);
+
    let products = [
+      {
+         linearBg: "orange",
+         image: img1,
+         description: "Adidas Falcon Shoes For men - 2022 Edition",
+         price: "$120.50",
+      },
+      {
+         linearBg: "green",
+         image: img2,
+         description: "Puma x Hot Wheels For men - 2022 Edition",
+         price: "$160.99",
+      },
+      {
+         linearBg: "red",
+         image: img3,
+         description: "Reebook | Red Dragon For men - 2022 Edition",
+         price: "$230.50",
+      },
+      {
+         linearBg: "blue",
+         image: img4,
+         description: "Nike & Just Do It For men - 2022 Edition",
+         price: "$420.00",
+      },
       {
          linearBg: "orange",
          image: img1,
@@ -48,7 +76,7 @@ const FeaturedProducts = () => {
             </div>
          </div>
 
-         <div className="products">
+         <div className="products" ref={ref} {...events}>
             {products.map(({ linearBg, image, description, price }, i) => {
                return (
                   <div className="products__prod" key={i}>
