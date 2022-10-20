1ae53d48ceb84cd3faf8b81d97a6e9b8fe1675e9
Xusniddin <narkulof@gmail.com>
2022-10-20 19:31:19 +0500

    hero slider completed and some style fixed, but hero slider mobile screen bug unfixed

--- src/pages/landingPage/header/Hero.jsx
+++ src/pages/landingPage/header/Hero.jsx
@@ -20,16 +20,6 @@ const data = [
 const Hero = () => {
    const [index, setIndex] = useState(0);
 
-   useEffect(() => {
-      const lastIndex = data.length - 1;
-      if (index > lastIndex) {
-         setIndex(0);
-      }
-      if (index < 0) {
-         setIndex(lastIndex);
-      }
-   }, [index]);
-
    return (
       <section className="hero" id="hero">
          <div className="hero__left">
@@ -59,18 +49,24 @@ const Hero = () => {
             </div>
          </div>
 
-         <div className="hero__right">
-            {data.map(({ customer, customerImg, sneakerImg }, i) => {
-               if (index === i) {
+         <div
+            className="hero__right carousel slide relative"
+            id="carouselExampleControls"
+            data-bs-ride="carousel"
+         >
+            <div class="carousel-inner relative w-full overflow-hidden">
+               {data.map(({ customer, customerImg, sneakerImg }, i) => {
                   return (
                      <div
                         className={`hero__right--img ${
-                           index === i
-                              ? "animate__slideInLeft animate__delay-0.2s"
-                              : "animate__slideOutLeft"
-                        } animate__animated`}
+                           i === index && "active"
+                        } carousel-item relative float-left w-full`}
                      >
-                        <img src={sneakerImg} alt="sneaker" />
+                        <img
+                           src={sneakerImg}
+                           alt="sneaker"
+                           class="block w-full"
+                        />
 
                         <div className="hero__right--img--notification">
                            <div className="hero__right--img--notification--flex">
@@ -88,14 +84,21 @@ const Hero = () => {
                         </div>
                      </div>
                   );
-               }
-            })}
+               })}
+            </div>
 
             <div className="hero__right--arrows">
-               <div onClick={() => setIndex((prev) => prev - 1)}>
+               <div
+                  data-bs-target="#carouselExampleControls"
+                  data-bs-slide="prev"
+               >
                   <LeftArrowCircleOutlinedIcon />
                </div>
-               <div onClick={() => setIndex((prev) => prev + 1)}>
+
+               <div
+                  data-bs-target="#carouselExampleControls"
+                  data-bs-slide="next"
+               >
                   <RightArrowCircleOutlinedIcon />
                </div>
             </div>
