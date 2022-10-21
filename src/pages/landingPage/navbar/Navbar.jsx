93a3145272e1cb88d7af9ac2b1f42f80f86875e7
Xusniddin <narkulof@gmail.com>
2022-10-21 17:50:57 +0500

    navbar style fixed

--- src/pages/landingPage/navbar/Navbar.jsx
+++ src/pages/landingPage/navbar/Navbar.jsx
@@ -42,19 +42,24 @@ const Navbar = () => {
                aria-label="Toggle navigation"
             >
                <svg
+                  width="32"
+                  height="12"
+                  fill="none"
+                  viewBox="0 0 32 12"
+                  xmlns="http://www.w3.org/2000/svg"
                   aria-hidden="true"
                   focusable="false"
                   data-prefix="fas"
                   data-icon="bars"
                   className="w-6"
                   role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
                >
                   <path
-                     fill="currentColor"
-                     d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-                  ></path>
+                     fill-rule="evenodd"
+                     clip-rule="evenodd"
+                     d="M1 0C0.447715 0 0 0.447715 0 1C0 1.55228 0.447715 2 1 2H31C31.5523 2 32 1.55228 32 1C32 0.447715 31.5523 0 31 0H1ZM1 10C0.447715 10 0 10.4477 0 11C0 11.5523 0.447715 12 1 12H31C31.5523 12 32 11.5523 32 11C32 10.4477 31.5523 10 31 10H1Z"
+                     fill="#0A083A"
+                  />
                </svg>
             </button>
             <div
@@ -74,13 +79,7 @@ const Navbar = () => {
        "
                   href="#"
                >
-                  <img
-                     // src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png"
-                     src={logo}
-                     style={{ height: "4rem" }}
-                     alt=""
-                     loading="lazy"
-                  />
+                  <img src={logo} alt="" loading="lazy" className="nav-logo" />
                </a>
             </div>
 
@@ -88,7 +87,7 @@ const Navbar = () => {
                className="collapse navbar-collapse flex-grow items-center"
                id="navbarSupportedContent"
             >
-               <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto gap-10 p-5">
+               <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto gap-10 p-5 items-center">
                   <li className="nav-item p-2">
                      <a
                         className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
@@ -123,11 +122,10 @@ const Navbar = () => {
                   </li>
                </ul>
             </div>
-
             {/* <!-- Collapsible wrapper --> */}
 
             {/* <!-- Right elements --> */}
-            <div className="flex items-center relative ml-4">
+            <div className="flex items-center relative ml-4 nav-right-max-991px">
                {/* <!-- Icon --> */}
                <a
                   className="text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-8"
@@ -135,31 +133,52 @@ const Navbar = () => {
                >
                   <Search />
                </a>
+
                <a
-                  className="text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-8"
+                  className="relative cart-icon text-gray-500 hover:text-gray-700 focus:text-gray-700 mr-8"
                   href="#"
                >
                   <svg
+                     width="24"
+                     height="24"
+                     viewBox="0 0 24 24"
+                     fill="none"
+                     xmlns="http://www.w3.org/2000/svg"
                      aria-hidden="true"
                      focusable="false"
                      data-prefix="fas"
                      data-icon="shopping-cart"
                      className="w-8"
                      role="img"
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 576 512"
                   >
                      <path
-                        fill="currentColor"
-                        d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
-                     ></path>
+                        d="M6 2L3 6V20C3 20.5304 3.21071 21.0391 3.58579 21.4142C3.96086 21.7893 4.46957 22 5 22H19C19.5304 22 20.0391 21.7893 20.4142 21.4142C20.7893 21.0391 21 20.5304 21 20V6L18 2H6Z"
+                        stroke="#0A083A"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                     />
+                     <path
+                        d="M3 6H21"
+                        stroke="#0A083A"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                     />
+                     <path
+                        d="M16 10C16 11.0609 15.5786 12.0783 14.8284 12.8284C14.0783 13.5786 13.0609 14 12 14C10.9391 14 9.92172 13.5786 9.17157 12.8284C8.42143 12.0783 8 11.0609 8 10"
+                        stroke="#0A083A"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                     />
                   </svg>
-                  <span className="text-white bg-red-700 absolute rounded-full text-sm -mt-2.5 ml-4 py-0 px-1.5">
-                     1
-                  </span>
+
+                  <span>1</span>
                </a>
+
                <div className="dropdown relative">
-                  <a
+                  {/* <a
                      className="
            text-gray-500
            hover:text-gray-700
@@ -193,7 +212,7 @@ const Navbar = () => {
                      <span className="text-white bg-red-700 absolute rounded-full text-sm -mt-2.5 ml-4 py-0 px-1.5">
                         1
                      </span>
-                  </a>
+                  </a> */}
                   <ul
                      className="
        dropdown-menu
